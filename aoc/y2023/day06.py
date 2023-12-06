7d6fd4bc2e525ec9220ef48317c95140baafaa7b
Adam Taylor <aktaylor08@gmail.com>
2023-12-06 09:32:25 -0600

    Added quad solve too

--- aoc/y2023/day06.py
+++ aoc/y2023/day06.py
@@ -1,5 +1,6 @@
 import numpy as np
 from aoc import get_input
+import math
 
 
 def go(time, record):
@@ -7,16 +8,26 @@ def go(time, record):
     return ((time - tarr) * tarr > record).sum()
 
 
+def thing(time, record):
+    min_time = math.ceil((time / 2) - (math.sqrt(time**2 - 4 * record) / 2))
+    max_time = math.floor((time / 2) + (math.sqrt(time**2 - 4 * record) / 2))
+    return max_time - min_time + 1
+
+
 times, distance = get_input(2023, 6).strip().split("\n")
 tlong = ""
 dlong = ""
 part1 = 1
+part1_2 = 1
 for t, d in zip(
     (int(x) for x in times.split(":")[1].strip().split()),
     (int(x) for x in distance.split(":")[1].strip().split()),
 ):
     part1 *= go(t, d)
+    part1_2 *= thing(t, d)
     tlong += str(t)
     dlong += str(d)
 print(part1)
+print(part1_2)
 print(go(int(tlong), int(dlong)))
+print(thing(int(tlong), int(dlong)))
