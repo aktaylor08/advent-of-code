fa862cc89344b83d5461d54e8fb496ce672e540f
Adam Taylor <aktaylor08@gmail.com>
2023-12-10 13:21:57 -0600

    working day 10

--- aoc/y2023/day10.py
+++ aoc/y2023/day10.py
@@ -1,4 +1,5 @@
 from aoc import get_input
+from shapely import Polygon, Point
 
 
 def get_possible_neighbors(
@@ -63,25 +64,42 @@ def parse_map(input: str) -> tuple[list[str], tuple[int, int]]:
 
 
 def travel(start, the_map):
-    queue = [start]
+    queue = [(start, None)]
     visited = {start: 0}
     dist = 1
+    strings = [[], []]
     while queue:
         new_queue = []
-        for node in queue:
-            for possible in get_valid_neighbors(node, the_map):
-                if possible not in visited:
-                    visited[possible] = dist
-                    new_queue.append(possible)
+        for node, line_no in queue:
+            valid = [n for n in get_valid_neighbors(node, the_map) if n not in visited]
+            if line_no is None and len(valid) == 2:
+                strings[0] = [node]
+                strings[1] = [node]
+                new_queue.append((valid[0], 0))
+                new_queue.append((valid[1], 1))
+                visited[valid[0]] = dist
+                visited[valid[1]] = dist
+            elif len(valid) == 1:
+                visited[valid[0]] = dist
+                strings[line_no].append(node)
+                new_queue.append((valid[0], line_no))
             queue = new_queue
         dist += 1
-    return visited, dist
+    return visited, dist, strings
 
 
 if __name__ == "__main__":
-    the_map, start = parse_map(get_input(2023, 10, sample=1))
-    dist_map, dist = travel(start, the_map)
+    the_map, start = parse_map(get_input(2023, 10))
+    dist_map, dist, strings = travel(start, the_map)
+    print(max(dist_map.values()))
+    poly = Polygon(strings[0] + strings[1][::-1])
+    print(poly.area)
+    count = 0
     for i in range(len(the_map)):
         for j in range(len(the_map[i])):
-            get_possible_neighbors((i, j), the_map)
-    print(max(dist_map.values()))
+            if (i, j) not in dist_map:
+                p = Point(i, j)
+                if poly.contains(p):
+                    print(i, j)
+                    count += 1
+    print(count)
