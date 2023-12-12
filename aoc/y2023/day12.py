abc075ccb2d0efd8925dc2df6fce7d5971dfdece
Adam Taylor <aktaylor08@gmail.com>
2023-12-12 16:06:24 -0600

    Working day 12

--- aoc/y2023/day12.py
+++ aoc/y2023/day12.py
@@ -1,5 +1,5 @@
-from ctypes.wintypes import DWORD
 from aoc import get_input
+from functools import cache
 
 
 class Grouping:
@@ -50,33 +50,49 @@ def possible_fits(count, line_str):
     # If there is leftover and it is a ? we have to flip it to '.' if it is a # it isn't possible
 
 
-def do_line(springs, counts) -> int:
-    if len(counts) == 0:
-        if "#" in springs:
-            return 0
-        else:
+@cache
+def do_line(springs, counts, pos=0, current=0) -> int:
+    if pos == len(springs):
+        if not counts and not current:
             return 1
-    first = counts[0]
-    rest = counts[1:]
-    total_sub = 0
-    for idx in range(len(springs)):
-        next_spring = possible_fits(first, springs[idx:])
-        if next_spring is not None:
-            total_sub += do_line(next_spring, rest)
-    return total_sub
+        elif len(counts) == 1 and counts[0] == current:
+            return 1
+        else:
+            return 0
+
+        return not counts and not current
+    if not counts:
+        return "#" not in springs[pos:]
+    chars = springs[pos]
+    values = 0
+    if chars in ["?", "."]:
+        # A good spot
+        if current != 0:
+            if len(counts) > 0 and current == counts[0]:
+                values += do_line(springs, counts[1:], pos + 1, current=0)
+        else:
+            values += do_line(springs, counts, pos + 1, current=0)
+    if chars in ["?", "#"]:
+        values += do_line(springs, counts, pos + 1, current + 1)
+        # A bad spot
+    return values
 
 
 def main():
     sum = 0
-    for line in get_input(2023, 12, 1).split("\n"):
+    sum2 = 0
+    for line in get_input(2023, 12).split("\n"):
         if line.strip() == "":
             continue
         springs, counts = line.split()
         counts = [int(x) for x in counts.strip().split(",")]
-        a = do_line(springs, counts)
-        print(a, line)
-        sum += a
-    do_line("?###????????", [3, 2, 1])
+        sum += do_line(springs, tuple(counts))
+        springs2 = "?".join(springs for x in range(5))
+        counts2 = counts * 5
+        sum2 += do_line(springs2, tuple(counts2))
+    print(sum)
+    print(sum2)
+    springs = ".#"
 
 
 if __name__ == "__main__":
