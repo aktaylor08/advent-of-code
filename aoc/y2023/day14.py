0120c49e83ac73df3dabba59379edf16d951fa68
Adam Taylor <aktaylor08@gmail.com>
2023-12-14 11:32:05 -0600

    Day14 working

--- aoc/y2023/day14.py
+++ aoc/y2023/day14.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 from aoc import get_input
 
 
@@ -22,27 +23,84 @@ def tilt_down(rocks: list[list[str]]) -> list[list[str]]:
     rocks.reverse()
     return rocks
 
-def pr(rest);
+
+def tilt_left(rocks: list[list[str]]) -> list[list[str]]:
+    for row in range(len(rocks)):
+        for col in range(len(rocks[0])):
+            if rocks[row][col] in ["O", "#"]:
+                continue
+            for right in range(col, len(rocks[row])):
+                if rocks[row][right] == "#":
+                    break
+                if rocks[row][right] == "O":
+                    rocks[row][col] = "O"
+                    rocks[row][right] = "."
+                    break
+    return rocks
+
+
+def tilt_right(rocks: list[list[str]]) -> list[list[str]]:
+    for x in rocks:
+        x.reverse()
+    tilt_left(rocks)
+    for x in rocks:
+        x.reverse()
+    return rocks
+
+
+def spin(rocks):
+    rocks = tilt_up(rocks)
+    rocks = tilt_left(rocks)
+    rocks = tilt_down(rocks)
+    rocks = tilt_right(rocks)
+    rock_locs = set()
+    for r in range(len(rocks)):
+        for c in range(len(rocks[0])):
+            if rocks[r][c] == "O":
+                rock_locs.add((r, c))
+    return rocks, rock_locs
+
+
+def pr(rest):
+    print("------")
     for x in rest:
         print(x)
 
+
+def weight(rock_locs, size):
+    sum = 0
+    for row, _ in rock_locs:
+        sum += size - row
+    return sum
+
+
 if __name__ == "__main__":
-    input = [
-        [c for c in "O....#...."],
-        [c for c in "O.OO#....#"],
-        [c for c in ".....##..."],
-        [c for c in "OO.#O....O"],
-        [c for c in ".O.....O#."],
-        [c for c in "O.#..O.#.#"],
-        [c for c in "..O..#O..O"],
-        [c for c in ".......O.."],
-        [c for c in "#....###.."],
-        [c for c in "#OO..#...."],
-    ]
-    # input = [[c for c in x] for x in get_input(2023, 14).split("\n")][:-1]
+    input = [[c for c in x] for x in get_input(2023, 14).split("\n")][:-1]
     rest = tilt_up(input)
-    pr(rest)
     sum1 = 0
     for i, x in enumerate(rest):
         sum1 += sum([r == "O" for r in x]) * (len(rest) - i)
+    rocks = input
+    weights = {}
+    history = []
+    last = None
+    while True:
+        rocks, rock_locs = spin(rocks)
+        a_hash = hash(tuple(rock_locs))
+        if a_hash in weights:
+            last = a_hash
+            break
+        else:
+            weights[a_hash] = weight(rock_locs, len(rocks))
+        history.append(a_hash)
+    cycle_start = history.index(last)
+    cycle_size = len(history) - cycle_start
+    total = 1000000000
+    cycle_count = (total - cycle_start) // cycle_size
+    end_full_cycle = cycle_start + (cycle_size * cycle_count)
+    remaining = total - end_full_cycle
+    result = 0
+    for x in range(remaining):
+        result = weights[history[x + cycle_start]]
     print(sum1)
+    print(result)
