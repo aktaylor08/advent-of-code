80689bdb7f966e26f45ed4c7da0adad66bf51fa0
Adam Taylor <aktaylor08@gmail.com>
2023-12-19 11:50:28 -0600

    not working part2

--- aoc/y2023/day19.py
+++ aoc/y2023/day19.py
@@ -26,6 +26,32 @@ class Workflow:
                     raise Exception("NO check in rule?")
         return None
 
+    def by_set(self, input_sets):
+        result_sets = {}
+        for rule in self.rules:
+            if ":" not in rule:
+                res_dict = {x: set(v) for x, v in input_sets.items()}
+                result_sets[rule] = res_dict
+            else:
+                check, dest = rule.split(":")
+                if "<" in rule:
+                    left, right = check.split("<")
+                    res_dict = {x: set(v) for x, v in input_sets.items()}
+                    the_split = set(filter(lambda x: x < int(right), input_sets[left]))
+                    res_dict[left] = the_split
+                    input_sets[left] = input_sets[left] - the_split
+                    result_sets[dest] = res_dict
+                elif ">" in rule:
+                    left, right = check.split(">")
+                    res_dict = {x: set(v) for x, v in input_sets.items()}
+                    the_split = set(filter(lambda x: x > int(right), input_sets[left]))
+                    res_dict[left] = the_split
+                    input_sets[left] = input_sets[left] - the_split
+                    result_sets[dest] = res_dict
+                else:
+                    raise Exception("NO check in rule?")
+        return result_sets
+
 
 def parse_rule(aline: str) -> Workflow:
     name, rest = aline.split("{")
@@ -64,15 +90,55 @@ def process(workflows: dict[str, Workflow], inparts: list[dict[str, int]]):
             sum1 += part["m"]
             sum1 += part["a"]
             sum1 += part["s"]
-    print(sum1)
+    return sum1
+
+
+def part_2(workflows):
+    start = 1
+    end = 4001
+    inputs = [
+        (
+            "in",
+            {
+                "x": set([x for x in range(start, end)]),
+                "m": set([x for x in range(start, end)]),
+                "a": set([x for x in range(start, end)]),
+                "s": set([x for x in range(start, end)]),
+            },
+        )
+    ]
+    input_vals = {a[0] for a in inputs}
+    while input_vals != set(["R", "A"]):
+        next_inputs = []
+        for w, to_compute in inputs:
+            if w not in ["A", "R"]:
+                worker = workflows[w]
+                res = worker.by_set(to_compute)
+            else:
+                res = {w: to_compute}
+            for dest in res:
+                next_inputs.append((dest, res[dest]))
+        inputs = next_inputs
+        input_vals = {a[0] for a in inputs}
+    print("-------")
+    result = 1
+    for x in inputs:
+        if x[0] == "A":
+            print(x[1])
+            temp = 1
+            for v in "xmas":
+                temp *= len(x[1][v])
+            result += temp
+    print(result)
 
 
 def main():
-    input = get_input(2023, 19)
+    input = get_input(2023, 19, 0)
     workflows, parts = input.split("\n\n")
     workflows = get_work(workflows)
     parts = get_parts(parts)
     process(workflows, parts)
+    part_2(workflows)
 
 
 if __name__ == "__main__":
