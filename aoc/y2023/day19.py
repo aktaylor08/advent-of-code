1593953bb0ddea0666789be2dbc77bdc43db8a6a
Adam Taylor <aktaylor08@gmail.com>
2023-12-19 21:59:07 -0600

    Working

--- aoc/y2023/day19.py
+++ aoc/y2023/day19.py
@@ -39,21 +39,19 @@ class Workflow:
                 if "<" in check:
                     left, right = check.split("<")
                     valid = still_valid[left]
-                    good_range = (valid[0], int(right))
+                    good_range = (valid[0], int(right) - 1)
                     rest_range = (int(right), valid[1])
-                    new_dict = dict(**still_valid)
-                    new_dict[left] = good_range
-                    outputs[dest] = new_dict
-                    still_valid[left] = rest_range
+
                 elif ">" in check:
                     left, right = check.split(">")
                     valid = still_valid[left]
                     good_range = (int(right) + 1, valid[1])
                     rest_range = (valid[0], int(right))
-                    new_dict = dict(**still_valid)
-                    new_dict[left] = good_range
-                    outputs[dest] = new_dict
-                    still_valid[left] = rest_range
+                print(check, good_range, rest_range)
+                new_dict = dict(**still_valid)
+                new_dict[left] = good_range
+                outputs[dest] = new_dict
+                still_valid[left] = rest_range
         return outputs
 
     def process(self, part):
@@ -103,44 +101,52 @@ def get_parts(input_str) -> list[dict[str, int]]:
 
 def process(workflows: dict[str, Workflow], inparts: list[dict[str, int]]):
     sum1 = 0
+    sum2 = 0
     for part in inparts:
         wf = "in"
         while wf not in ["R", "A"]:
             worker = workflows[wf]
             wf = worker.process(part)
         if wf == "A":
-            sum1 += part["x"]
-            sum1 += part["m"]
-            sum1 += part["a"]
-            sum1 += part["s"]
-    return sum1
+            sum2 += 1
+            for x in part:
+                sum1 += part[x]
+    return sum1, sum2
 
 
 def part_2point2(workflows, input_set="xy", minv=1, maxv=10):
     input_ranges = {x: (minv, maxv) for x in input_set}
     input_things = [("in", input_ranges, [], 0)]
     sum = 0
-    ranges = []
+    sum2 = 0
     while input_things:
-        w, in_data, path, depth = input_things[0]
-        print(depth)
-        input_things = input_things[1:]
+        w, in_data, path, depth = input_things.pop()
         if w not in ["R", "A"]:
             worker = workflows[w]
             for dest, inputs in worker.split_ranges(in_data).items():
                 input_things.append((dest, inputs, path[:] + [w], depth + 1))
         elif w == "A":
             temp = 1
-            print(depth, path)
-            for x in in_data:
-                print("\t", x, in_data[x])
+            print("------->")
             for k, (minv, maxv) in in_data.items():
-                print(maxv - minv)
+                print(k, (maxv - minv + 1))
                 temp = temp * (maxv - minv + 1)
-            ranges.append(temp)
+            print("<<<<")
+            print(temp)
+            print("=======")
             sum += temp
-    print(ranges)
+        elif w == "R":
+            temp = 1
+            for k, (minv, maxv) in in_data.items():
+                print(k, (maxv - minv + 1))
+                temp = temp * (maxv - minv + 1)
+            sum2 += temp
     print(sum)
+    print(sum2)
+    print(sum + sum2)
+    a = 4000 * 4000 * 4000 * 4000
+    print(a)
+    print(a - sum2)
 
 
 def main():
@@ -148,12 +154,21 @@ def main():
     workflows, parts = input.split("\n\n")
     workflows = get_work(workflows)
     parts = get_parts(parts)
+    print(process(workflows, parts))
     part_2point2(workflows, input_set="xmas", minv=1, maxv=4000)
-    workflows = {
-        "in": Workflow("in", ["x<5:one", "two"]),
-        "one": Workflow("in", ["y>7:A", "R"]),
-        "two": Workflow("in", ["y<3:A", "R"]),
-    }
+    # workflows = {
+    # "in": Workflow("in", ["x<5:one", "z>4:A", "two"]),
+    # "one": Workflow("in", ["y>7:A", "z<3:three", "R"]),
+    # "two": Workflow("in", ["y<3:A", "R"]),
+    # "three": Workflow("in", ["z>3:R", "y>8:A", "R"]),
+    # }
+    # parts = []
+    # for x in range(1, 11):
+    # for y in range(1, 11):
+    # for z in range(1, 11):
+    # parts.append({"x": x, "y": y, "z": z})
+    # print(process(workflows, parts))
+    # part_2point2(workflows, input_set="xyz", minv=1, maxv=10)
 
 
 if __name__ == "__main__":
