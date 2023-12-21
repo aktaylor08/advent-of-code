e41de034c1d9ea431d8a2d81389caad00e1381ab
Adam Taylor <aktaylor08@gmail.com>
2023-12-21 11:48:04 -0600

    workign slow

--- aoc/y2023/day21.py
+++ aoc/y2023/day21.py
@@ -2,12 +2,15 @@ from aoc import get_input
 
 
 def search(
-    amap, row, col, depth, complete, history, oob, off_down=0, off_right=0
-) -> int:
+    amap, row, col, depth, complete, history, oob_history, off_down=0, off_right=0
+) -> tuple[int, set]:
     sum = 0
+    oob = set()
     complete.add((row, col, depth, off_down, off_right))
     if (row, col, depth, off_down, off_right) in history:
-        return history[(row, col, depth, off_down, off_right)]
+        return history[(row, col, depth, off_down, off_right)], oob_history[
+            (row, col, depth, off_down, off_right)
+        ]
     if depth == 0:
         sum += 1
     else:
@@ -19,26 +22,30 @@ def search(
                     amap[newr][newc] in [".", "S"]
                     and (newr, newc, depth - 1, off_down, off_right) not in complete
                 ):
-                    sum += search(
+                    value, new_oob = search(
                         amap,
                         newr,
                         newc,
                         depth - 1,
                         complete,
                         history,
-                        oob,
+                        oob_history,
                         off_down,
                         off_right,
                     )
+                    oob.update(new_oob)
+                    sum += value
             else:
                 oob.add((newr, newc, depth - 1, off_down, off_right))
     if (row, col, depth, off_down, off_right) not in history:
         history[(row, col, depth, off_down, off_right)] = sum
-    return sum
+    if (row, col, depth, off_down, off_right) not in oob_history:
+        history[(row, col, depth, off_down, off_right)] = set(oob)
+    return sum, set(oob)
 
 
 def main():
-    step_count = 100
+    step_count = 500
     amap = get_input(2023, 21, 0).strip().split()
     start = None
     for row, x in enumerate(amap):
@@ -46,10 +53,12 @@ def main():
         if col >= 0:
             start = (row, col)
     history = {}
-    oob = set()
     complete = set()
-    value = search(amap, start[0], start[1], step_count, complete, history, oob)
-    out_of_bounds = list(oob)
+    oob_history = {}
+    value, oob = search(
+        amap, start[0], start[1], step_count, complete, history, oob_history
+    )
+    out_of_bounds = [(o[0], o[1], o[2], o[3], o[4]) for o in oob]
     total = value
     while out_of_bounds:
         row, col, dist, off_down, off_right = out_of_bounds[0]
@@ -60,13 +69,21 @@ def main():
         if (row, col, dist, off_down, off_right) not in complete:
             if (row, col, dist, off_down, off_right) in history:
                 new_value = history[(row, col, dist, off_down, off_right)]
+                oob = []
             else:
-                oob = set()
-                new_value = search(
-                    amap, row, col, dist, complete, history, oob, off_down, off_right
+                new_value, oob = search(
+                    amap,
+                    row,
+                    col,
+                    dist,
+                    complete,
+                    history,
+                    oob_history,
+                    off_down,
+                    off_right,
                 )
-                for x in oob:
-                    out_of_bounds.append(x)
+            for x in oob:
+                out_of_bounds.append(x)
             total += new_value
     print(total)
 
